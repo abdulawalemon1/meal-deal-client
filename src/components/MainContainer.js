1a16a75d111865a466b725fe673ee0087beaf9b9
abdulawalemon1 <emondx4@gmail.com>
2022-07-19 02:33:02 +0600

    right banner added

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import Delivery from "../img/delivery.png";
+import HomeContainer from './HomeContainer';
 
 const MainContainer = () => {
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-2'>
-            <div className='py-2 flex-1 flex flex-col items-start justify-center gap-6'>
-                <div className='flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full'>
-                    <p className='text-base text-orange-500 font-semibold'>Bike Delivery</p>
-                    <div className='bg-white w-8 h-8 rounded-full overflow-hidden drop-shadow-xl'>
-                        <img src={Delivery} className='w-full h-full object-contain' alt="delivery" />
-                    </div>
-                </div>
-
-                <p className='text-[2.5rem] md:text-[4.5rem] font-bold tracking-wide text-headingColor'>The Fastest Delivery in <span className='text-orange-600 text-[3rem] md:text-[5rem]'>Your City</span></p>
-
-                <p className='text-base text-textColor text-center md:text-left'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quam illum at optio veniam labore fugit et. Alias cupiditate inventore odit explicabo blanditiis? Non explicabo veniam omnis impedit commodi voluptate!</p>
-
-                <button type='button' className='bg-gradient-to-br from-orange-600 w-full md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100 font-semibold'>Order Now!</button>
-            </div>
-            <div className='py-2 flex-1'></div>
+        <div className='w-full h-auto flex flex-col items-center justify-center'>
+            <HomeContainer></HomeContainer>
         </div>
     );
 };
---
+++ src/utils/data.js
@@ -0,0 +1,12 @@
+import r2 from "../img/r2.png";
+import cu4 from "../img/cu4.png";
+import fi3 from "../img/fi3.png";
+import f1 from "../img/f1.png";
+
+
+export const heroData = [
+    { id: 1, name: 'Biriyani Platter', decp: 'Hydrabadi Dom', price: '8.5', imgSrc: r2 },
+    { id: 2, name: 'Chicken Curry', decp: 'Spicy Chicken ', price: '5.5', imgSrc: cu4 },
+    { id: 3, name: 'Tuna BBQ', decp: 'Fish BBQ + Salad', price: '9.5', imgSrc: fi3 },
+    { id: 4, name: 'Strawberries', decp: 'Fresh Fruits', price: '2.75', imgSrc: f1 },
+]
